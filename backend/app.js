234aa13ce81b77d864674a6411acac485f3178c0
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-07-29 00:52:13 +0100

    installation express

---
+++ backend/app.js
@@ -0,0 +1,4 @@
+const  express = require('express'); // Import the express module to create an Express application
+const app = express(); // Create an instance
+
+module.exports = app; // Export the app instance for use in other files
