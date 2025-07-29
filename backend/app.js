d4a2ab24a5f7c02e0cf809f90d2ab9c24db90700
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-07-29 01:27:53 +0100

    amelioration du fichier server

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const  express = require('express'); // Import the express module to create an Express application
 const app = express(); // Create an instance
-
+app.use((req, res, next) => {
+    console.log("Request received"); // Log the HTTP method and URL of the request
+    next(); // Call the next middleware in the stack
+})
+// Middleware to log the request method and URL
+app.use((req, res, next) => {
+    res.status(201); // Set the response status code to 201 (Created)
+    next(); // Call the next middleware in the stack
+})
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a été reçue avec succès!' }); // Respond with a JSON message
+    next(); // Call the next middleware in the stack
+})
+// Middleware to handle all requests and respond with a JSON message
 app.use((req, res) => {
-    res.json({ message: 'Hello World!' }); // Respond with a JSON message
+    console.log("Response sent"); // Log that the response has been sent
 })
 module.exports = app; // Export the app instance for use in other files
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,47 @@
-const http = require('http'); // Import the http module to create an HTTP server
-const app = require('./app'); // Import the Express application instance
+const http = require('http');
+const app = require('./app');
 
-app.set('port', process.env.PORT || 3000); // Set the port for the application, defaulting to 3000 if not specified
-// Create an HTTP server using the Express application instance
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT ||'3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
