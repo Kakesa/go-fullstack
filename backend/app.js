87d5acd4740fba0cb13dc1cde0ac2c46ae35dbca
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-07-29 05:26:44 +0100

    ajout de mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,37 @@
-const  express = require('express'); // Import the express module to create an Express application
+const express = require('express'); // Import the express module to create an Express application
+const mongoose = require('mongoose'); // Import mongoose for MongoDB interactions
+
+// Connect to MongoDB using Mongoose
+mongoose.connect('mongodb+srv://espoirkakesa2:JehovahDieu1@cluster0.9dujdei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express(); // Create an instance
 
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+// Middleware to set CORS headers
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-app.use('/api/stuff', (req, res, next) => {
+
+// Define a simple route for the root path
+app.post('/api/stuff', (req, res) => {
+    console.log(req.body);
+    if (!req.body.title) {
+        return res.status(400).json({ message: 'Titre manquant !' });
+    }
+    res.status(201).json({ 
+        message: 'Objet créé !' 
+    });
+});
+
+app.get('/api/stuff', (req, res) => {
   const stuff = [
     {
       _id: 'oeihfzeoi',
@@ -28,4 +52,5 @@ app.use('/api/stuff', (req, res, next) => {
   ];
   res.status(200).json(stuff);
 });
+
 module.exports = app; // Export the app instance for use in other files
---
+++ backend/models/Thing.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose'); // Import mongoose for MongoDB interactions
+
+const thingSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    price: { type: Number, required: true },
+    userId: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
