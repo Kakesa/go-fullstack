3bbcf67de77f58892c3d9a618ccfd8387ffc27e2
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-07-29 09:15:26 +0100

    ajout de la modification et la suppression

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,25 @@ app.post('/api/stuff', (req, res) => {
         .catch(error => res.status(400).json({ error }));
 });
 
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+// Define a route to update a thing by its ID
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+// Define a route to delete a thing by its ID
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+// Define a route to get all things from the database
 app.get('/api/stuff', (req, res) => {
     Thing.find()
         .then(things => res.status(200).json(things))
