4fd0d567f63e5383ad063d0798128558708c6463
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-09-12 05:10:32 +0100

    fix : suppression du plugin mongoose-unique-validator et ajout des routes d'authentification

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');// Import the mongoose library
-const uniqueValidator = require('mongoose-unique-validator');// Import the uniqueValidator plugin
 
 // Define the User schema with email and password fields
 const userSchema = mongoose.Schema({
@@ -8,5 +7,4 @@ const userSchema = mongoose.Schema({
 });
 
 // Apply the uniqueValidator plugin to userSchema
-userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('User', userSchema);
