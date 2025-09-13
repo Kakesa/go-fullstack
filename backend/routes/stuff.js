9b8660fe9a097ae2cd70a9793ec53ef33471bfad
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-09-13 05:17:59 +0100

    fix : add authentication middleware to protected routes

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,19 +1,20 @@
  const express = require('express'); // Import the express module to create an Express application
+ const auth = require('../middleware/auth'); // Import the authentication middleware
  const router = express.Router(); // Create a router instance
 
  const staffController = require('../controllers/stuff'); // Import the stuff controller
 
 
  // Define a simple route for the root path
-router.post('/', staffController.createThing);
+router.post('/', auth, staffController.createThing);
 // Define a route to get a single thing by its ID
-router.get('/:id', staffController.getOneThing);
+router.get('/:id', auth, staffController.getOneThing);
 
 // Define a route to update a thing by its ID
-router.put('/:id', staffController.modifyThing);
+router.put('/:id', auth, staffController.modifyThing);
 // Define a route to delete a thing by its ID
-router.delete('/:id', staffController.deleteThing);
+router.delete('/:id', auth, staffController.deleteThing);
 // Define a route to get all things from the database
-router.get('/', staffController.getAllThings);
+router.get('/', auth, staffController.getAllThings);
 
 module.exports = router; // Export the router instance for use in other files
